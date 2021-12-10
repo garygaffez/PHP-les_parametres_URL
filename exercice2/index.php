1fe9958587ed66c930a993c457a507aaf0fb2ab9
garygaffez <gary.gaffez@gmail.com>
2021-12-10 15:36:15 +0100

    debut parametre URL

---
+++ exercice2/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>partie 1 exercice 1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+</head>
+<body class="bg-dark">
+
+<div class="text-warning text-center fs-3 p-5">
+
+<!-- <form action="index.php" method="GET">
+    <div class="mb-3">
+        <label for="lastname" class="form-label">lastname</label>
+        <input type="text" class="form-control" name="lastname">
+    </div>
+    <div class="mb-3">
+        <label for="firstname" class="form-label">firstname</label>
+        <input type="text" class="form-control" name="firstname">
+    </div>
+    <button type="submit" class="btn btn-warning">Envoyer</button>
+</form> -->
+<?php
+
+    if (!isset($_GET['age'])) { ?>
+        <p>
+            <?='Il faut un âge pour soumettre le formulaire.';?>
+        </p>
+<?php
+    } else {
+        foreach ($_GET as $value) { ?>
+            <p>
+                <?=$value;?>
+            </p>
+<?php
+        }
+    }
+
+?>
+
